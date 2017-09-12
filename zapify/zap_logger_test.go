881bb17c1709af5352a9d1893af58bd6e72e181d
Abhinav Gupta <abg@uber.com>
2017-09-12 16:53:51 -0700

    Move zap support to subpackage
    Depending on bark shouldn't impose a dependency on zap.
    
    This moves zap support into a zapify subpackage.

--- zapify/zap_logger_test.go
+++ zapify/zap_logger_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Uber Technologies, Inc.
+// Copyright (c) 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -16,7 +16,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package bark_test
+package zapify_test
 
 import (
 	"errors"
